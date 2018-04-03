22ff94559972e95ba4620b952fb21a2efc01a1bb
tinturia46 <davide.russo@hs-furtwangen.de>
2018-04-03 20:03:09 +0200

    Hinzufügen Ordner

---
+++ Aufgabe-1-1_russodav/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace Aufgabe_1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+       {
+            var eingb = args[1]; // Liest Eingabe (Durhcmesser)
+            double d = Convert.ToDouble(eingb); // Eingabe konvertieren
+            
+            
+            // Eingabe erkennen und nach cases sortieren
+            switch (args[0])
+            {
+                case "w":
+                    getCubeInfo(d);
+                    break;
+
+                case "k":
+                    getSphereInfo(d);
+                    break;
+
+                case "o":
+                    getOctaInfo(d);
+                    break;
+            }
+        }
+        public static double getCubeSurface(double wd)
+        {
+            double a = 6 * (wd * wd);
+            return a;
+        }
+        public static double getCubeVolume(double wd)
+        {
+            double v = Math.Pow(wd, 3);
+            return v;
+        }
+        public static double getSphereSurface(double sd)
+        {
+            double a = (Math.PI * (sd * sd));
+            return a;
+        }
+        public static double getSphereVolume(double sd)
+        {
+            double v = (Math.PI * (Math.Pow(sd, 3) / 6));
+            return v;
+        }
+        public static double getOctaSurface(double od)
+        {
+            double a = 2 * (Math.Sqrt(3) * (od * od));
+            return a;
+        }
+        public static double getOctaVolume(double od)
+        {
+            double v = (Math.Sqrt(2) * (Math.Pow(od, 3) / 3));
+            return v;
+        }
+        public static void getCubeInfo(double d)
+        {
+            double a = getCubeSurface(d);
+            double v = getCubeVolume(d);
+
+            Console.WriteLine("Würfel : A = " + a + "V = " + v);
+
+        }
+        public static void getSphereInfo(double d)
+        {
+            double a = getSphereSurface(d);
+            double v = getSphereVolume(d);
+
+            Console.WriteLine("Kugel: A = " + a + "V = " + v);
+
+        }
+        public static void getOctaInfo(double d)
+        {
+            double a = getOctaSurface(d);
+            double v = getOctaVolume(d);
+
+            Console.WriteLine("Oktaeder: A = " + a + "V = " + v);
+        }
+    }
+
+}
---
+++ Aufgabe-1-2_russodav/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Aufgabe_1_2
+{
+     class Program{
+        static string[] subjects = { "Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore" };
+        static string[] verbs = { "braut", "liebt", "studiert", "hasst", "zaubert", "zerstört" };
+        static string[] objects = { "Zaubertränke", "den Grimm", "Lupin", "Hogwards", "die Karte des Rumtreibers", "Dementoren" };
+        static string subject;
+        static string verb;
+        static string object_1;
+        static int laenge = subjects.Length;        
+        static void Main(string[] args){
+
+        string[] verse = new string[laenge];
+
+            for (int i = 0; i < laenge; i++)
+            {
+                VerseCreator();
+                verse[i] = subject + " "+ verb + " " + object_1;
+            }
+
+            for (int i = 0; i < subjects.Length; i++){
+                Console.WriteLine(verse[i]);
+            }
+
+        }
+        public static void VerseCreator()
+        {
+            Random randomNumber = new Random();
+            int subjectsNumber = randomNumber.Next(0, laenge);
+            int verbsNumber = randomNumber.Next(0, laenge);
+            int objectsNumber = randomNumber.Next(0, laenge);
+
+
+            while (subjects[subjectsNumber] == "assigned")
+            {
+                subjectsNumber = randomNumber.Next(0, laenge);
+            }
+            subject = subjects[subjectsNumber];
+            subjects[subjectsNumber] = "assigned";
+
+
+            while (verbs[verbsNumber] == "assigned")
+            {
+                verbsNumber = randomNumber.Next(0, laenge);
+            }
+            verb = verbs[verbsNumber];
+            verbs[verbsNumber] = "assigned";
+            
+
+            while (objects[objectsNumber] == "assigned")
+            {
+                objectsNumber = randomNumber.Next(0, laenge);
+            }
+            object_1 = objects[objectsNumber];
+            objects[objectsNumber] = "assigned";
+
+        }
+    }
+}
