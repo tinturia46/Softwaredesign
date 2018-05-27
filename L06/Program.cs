9a97d15ec0441242cba4dd83410d12b052fb90ba
tinturia46 <davide.russo@hs-furtwangen.de>
2018-05-27 15:09:37 +0200

    Program.cs

---
+++ L06/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace L06
+{
+    class Program
+    {
+        public class Person
+        {
+            public string name;
+            public int age;
+
+        }
+        public class Prof : Person
+        {
+            public string consult;
+            public int room;
+        }
+
+        public class Participant : Person
+        {
+            public int number;
+        }
+
+        public class Course
+        {
+            int room;
+            string time;
+            string title;
+            public Participant participant;
+            public Prof prof;
+
+        }
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
